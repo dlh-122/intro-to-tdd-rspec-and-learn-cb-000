fad5aed2e7e1be203ec6bb3c4b5697f7e5a794a3
Diana Hayes <hayes12288@gmail.com>
2017-12-23 04:05:56 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -5,6 +5,6 @@ describe "current_age_for_birth_year method" do
     age_of_person = current_age_for_birth_year(1984)
 
     expect(age_of_person).to eq(19)
-  end
-def current_age_for_birth_year(birth_year)end
+  endend
+def current_age_for_birth_year(birth_year)
 end
